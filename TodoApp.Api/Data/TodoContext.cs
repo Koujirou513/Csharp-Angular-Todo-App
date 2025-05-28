8905a18b9543f70bc7a45fb8551030ebc6edcb4d
Koujirou513 <kojiro323a@gmail.com>
2025-05-29 08:41:12 +0900

    [add] Dataディレクトリ作成

---
+++ TodoApp.Api/Data/TodoContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Api.Models;
+
+namespace TodoApp.Api.Data;
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Todo> Todos { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Todo>().HasKey(t => t.Id);
+        modelBuilder.Entity<Todo>().Property(t => t.Title).IsRequired().HasMaxLength(200);
+        modelBuilder.Entity<Todo>().Property(t => t.Description).HasMaxLength(1000);
+    }
+}
